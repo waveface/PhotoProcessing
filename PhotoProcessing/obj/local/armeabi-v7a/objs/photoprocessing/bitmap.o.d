65c6565de031dcc55288b04bcf5c7886d829ea6e
Chris Banes <chris@senab.co.uk>
2012-07-11 17:02:17 +0100

    Clean and re-build JNI native code

--- PhotoProcessing/obj/local/armeabi-v7a/objs/photoprocessing/bitmap.o.d
+++ PhotoProcessing/obj/local/armeabi-v7a/objs/photoprocessing/bitmap.o.d
@@ -1,95 +1,95 @@
 obj/local/armeabi-v7a/objs/photoprocessing/bitmap.o: jni/bitmap.c \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/jni.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/math.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/types.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/stdint.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/_types.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/_types.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/types.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/types.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/limits.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/limits.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/limits.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/limits.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/android/log.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/stdio.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/stdlib.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/string.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/malloc.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/alloca.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/strings.h \
- /home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/memory.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/jni.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/math.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/limits.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/android/log.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdio.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/string.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/strings.h \
+ /Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/memory.h \
  jni/mem_utils.h jni/bitmap.h jni/transform.h
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/jni.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/jni.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/math.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/math.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/types.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/types.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/stdint.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdint.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/_types.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/_types.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/types.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/types.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/types.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/types.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/limits.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/limits.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/limits.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/limits.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/limits.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/android/log.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/android/log.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/stdio.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdio.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/stdlib.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdlib.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/string.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/string.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/malloc.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/malloc.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/alloca.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/alloca.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/strings.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/strings.h:
 
-/home/nilesh/Android/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/memory.h:
+/Users/chris/Apps/android-ndk-r7/platforms/android-14/arch-arm/usr/include/memory.h:
 
 jni/mem_utils.h:
 
